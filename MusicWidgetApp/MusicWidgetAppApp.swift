6563f8a9aebd650f12c658ecc55c1c4d6d4e59d8
Bhuvan Shah <bhuvanshah288@gmail.com>
2025-02-23 15:40:30 -0800

    Initial Commit

---
+++ MusicWidgetApp/MusicWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicWidgetAppApp.swift
+//  MusicWidgetApp
+//
+//  Created by Bhuvan Shah on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
