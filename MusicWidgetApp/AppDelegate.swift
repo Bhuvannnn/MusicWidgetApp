13259a2dcf2421074e8e1528a22cbb28e9117df7
Bhuvan Shah <bhuvanshah288@gmail.com>
2025-02-24 19:26:48 -0800

    nothing

--- MusicWidgetApp/AppDelegate.swift
+++ MusicWidgetApp/AppDelegate.swift
@@ -8,12 +8,19 @@ extension NSNotification.Name {
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Hide the main app window after launch
         NSApp.setActivationPolicy(.accessory)
     }
     
     func applicationWillTerminate(_ notification: Notification) {
-        // Restore activation policy when quitting
         NSApp.setActivationPolicy(.regular)
+    }
+    
+    // Add this function to handle URL schemes
+    func application(_ application: NSApplication, open urls: [URL]) {
+        guard let url = urls.first else { return }
+        
+        if url.scheme == "musicwidget" {
+            SpotifyAuth.shared.handleRedirect(url: url)
+        }
     }
 }
